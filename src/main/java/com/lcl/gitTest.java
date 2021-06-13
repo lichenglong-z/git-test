7e12f1bf3a400c1360c0d88abfef291c586806ba
lcl <894374336@qq.com>
2021-06-13 10:47:46 +0800

    hot-fix commit1

--- src/main/java/com/lcl/gitTest.java
+++ src/main/java/com/lcl/gitTest.java
@@ -5,5 +5,6 @@ public class gitTest {
         System.out.println("hello git!");
         System.out.println("second commit!");
         System.out.println("third commit!");
+        System.out.println("hot-fix commit");
     }
 }
