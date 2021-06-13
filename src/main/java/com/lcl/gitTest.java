c696fa3c33e314701166c8eebcebac23d52662a4
lcl <894374336@qq.com>
2021-06-13 09:58:51 +0800

    second commit

--- src/main/java/com/lcl/gitTest.java
+++ src/main/java/com/lcl/gitTest.java
@@ -3,5 +3,6 @@ package com.lcl;
 public class gitTest {
     public static void main(String[] args) {
         System.out.println("hello git!");
+        System.out.println("second commit!");
     }
 }
